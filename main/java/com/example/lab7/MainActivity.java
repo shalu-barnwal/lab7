c9f302f0a9fa90f65ab3b322034eb192c45cc0dc
shalu-barnwal <67273329+shalu-barnwal@users.noreply.github.com>
2021-01-25 13:16:29 +0530

    Add files via upload

---
+++ main/java/com/example/lab7/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lab7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName() ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
